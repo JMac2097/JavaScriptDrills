af52e4ba9f0e910bae8059f884d4fedca4c4c5d2
John <therealjmac2097@gmail.com>
2023-09-21 19:11:52 +0100

    Throttle added

--- Closures/scripts/task2.js
+++ Closures/scripts/task2.js
@@ -1,6 +1,8 @@
 const debounceBtn = document.querySelector(".js-debounce");
 const debounceFeedback = document.querySelector(".debounce-feedback");
 const debounceTestMsg = "Hello from debounce!";
+const throttleFeedback = document.querySelector(".throttle-feedback");
+const throttleTestMsg = "Hello from throttle!";
 
 const debounce = (func, delay) => {
   let inDebounce;
@@ -22,3 +24,24 @@ debounceBtn.addEventListener(
 );
 
 const throttleBtn = document.querySelector(".js-throttle");
+
+const throttle = (func, delay) => {
+  let lastCall = 0;
+  let timeoutId;
+
+  return function (...args) {
+    const now = Date.now();
+    if (now - lastCall >= delay) {
+      func(...args);
+      lastCall = now;
+    } else {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => func(...args), delay - (now - lastCall));
+    }
+  };
+};
+
+throttleBtn.addEventListener(
+  "click",
+  throttle(() => updateContent(throttleFeedback, throttleTestMsg), 1000)
+);
